83dcf3bbdf2771c8b3895892a699c60bc7bef295
ktemsupa <54868641+ktemsupa@users.noreply.github.com>
2019-10-24 18:18:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ import json
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
 
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -35,44 +34,14 @@ app.layout = html.Div([
     ),
     html.Br(),
     dcc.Graph(id='display-map'),
-    dcc.Graph(id='display-value'),
     html.Br(),
     html.A('Code on Github', href='https://github.com/ktemsupa/virginia_election_2016'),
     html.Br(),
     html.A('Data Source', href='https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LYWX3D')
 ])
 
-
-######### Callback #1 #########
-@app.callback(dash.dependencies.Output('display-value', 'figure'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def juris_picker(juris_name):
-    juris_df=df[df['jurisdiction']==juris_name]
-
-    mydata1 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Donald Trump']),
-                     marker=dict(color='#122A7F'),
-                     name='Trump')
-    mydata2 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Hillary Clinton']),
-                     marker=dict(color='#f96800'),
-                     name='Clinton')
-    mydata3 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Other']),
-                     marker=dict(color='#009900'),
-                     name='Other')
-
-    mylayout = go.Layout(
-        title='Votes by candidate for: {}'.format(juris_name),
-        xaxis=dict(title='Precincts'),
-        yaxis=dict(title='Number of Votes')
-    )
-    fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
-    return fig
-
-
-
-######### Callback #2 #########
+# #
+# # ######### Callback #1 #########
 @app.callback(dash.dependencies.Output('display-map', 'figure'),
               [dash.dependencies.Input('dropdown', 'value')])
 def juris_highlighter(juris_name):
@@ -92,7 +61,7 @@ def juris_highlighter(juris_name):
                       mapbox_center = {"lat": 38.0293, "lon": -79.4428})
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
-
+# #
 
 
 ######### Run the app #########
