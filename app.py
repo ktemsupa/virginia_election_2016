f6275960132a0a16cc69cbb711013b4c77fb074a
ktemsupa <54868641+ktemsupa@users.noreply.github.com>
2019-10-24 17:25:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,18 @@ import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
 from plotly.graph_objs import *
+import numpy as np
 
 ###### Import a dataframe #######
 df = pd.read_pickle('virginia_totals.pkl')
 options_list=list(df['jurisdiction'].value_counts().sort_index().index)
+
+# Read in the USA counties shape files
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -23,18 +31,19 @@ app.layout = html.Div([
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in options_list],
-        value=options_list[0]
+        value=options_list[10]
     ),
     html.Br(),
+    dcc.Graph(id='display-map'),
     dcc.Graph(id='display-value'),
     html.Br(),
-    html.A('Code on Github', href='https://github.com/austinlasseter/virginia_election_2016'),
+    html.A('Code on Github', href='https://github.com/ktemsupa/virginia_election_2016'),
     html.Br(),
     html.A('Data Source', href='https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LYWX3D')
 ])
 
 
-######### Interactive callbacks go here #########
+######### Callback #1 #########
 @app.callback(dash.dependencies.Output('display-value', 'figure'),
               [dash.dependencies.Input('dropdown', 'value')])
 def juris_picker(juris_name):
@@ -60,6 +69,30 @@ def juris_picker(juris_name):
     )
     fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
     return fig
+
+
+
+######### Callback #2 #########
+@app.callback(dash.dependencies.Output('display-map', 'figure'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def juris_highlighter(juris_name):
+    df['selected']=np.where(df['jurisdiction']==juris_name, 1, 0)
+    fig = go.Figure(go.Choroplethmapbox(geojson=counties,
+                                        locations=df['FIPS'],
+                                        z=df['selected'],
+                                        # colorscale=['blues'],
+                                        text=df['county_name'],
+                                        hoverinfo='text',
+                                        zmin=0,
+                                        zmax=1,
+                                        marker_line_width=.5
+                                        ))
+    fig.update_layout(mapbox_style="carto-positron",
+                      mapbox_zoom=5.8,
+                      mapbox_center = {"lat": 38.0293, "lon": -79.4428})
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
 
 
 ######### Run the app #########
